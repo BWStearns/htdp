caabddae76c9c2999b20847d8d24d880e1f812fc
Brian Stearns <brianw.stearns@gmail.com>
2016-03-19 22:16:48 -0400

    Got sidetracked by making a nice graduated tax function.

--- src/htdp/ch1/ch1.clj
+++ src/htdp/ch1/ch1.clj
@@ -4,85 +4,104 @@
   "Skipping all of the exercises for which the answer is not a function. This
   is to enable testing. Maybe I'll go back and add maps of {ex# ans} later?")
 
-(defn f->c [t]
+(defn f->c
   "Ex 2.2.1"
+  [t]
   (/ (- t 32) 1.8))
 
-(defn dollar->euro [d]
+(defn dollar->euro
   "Ex 2.2.2"
+  [d]
   (* d 0.89))
 
-(defn triangle [b h]
+(defn triangle
   "Ex 2.2.3"
+  [b h]
   (/ (* b h) 2))
 
-(defn convert3 [ones tens hundreds]
+(defn convert3
   "Ex 2.2.4"
+  [ones tens hundreds]
   (+ (* 100 hundreds)
      (* 10 tens)
      ones))
 
-(defn tax [gross]
+(defn tax
   "Ex 2.3.1"
+  [gross]
   (* gross 0.15))
 
-(defn net [hours]
+(defn net
   "Ex 2.3.1"
+  [hours]
   (let [gross (* hours 12)]
     (- gross (tax gross))))
 
-(defn sum-coins [counts]
+(defn sum-coins
   "Ex 2.3.2"
+  [counts]
   (reduce-kv (fn [total value n] (+ total (* value n)))
              0
              (zipmap [1 5 10 25] counts)))
 
-(defn total-profit [n]
+(defn total-profit
   "Ex 2.3.3"
+  [n]
   (- (* 4.5 n) 20))
 
 ;; The functions for the movie theater examples
 
-(defn attendees [ticket-price]
+(defn attendees
   "Ex 3.1.3-4"
+  [ticket-price]
   (+ 120
      (* (/ 15 0.10) (- 5 ticket-price))))
 
-(defn cost [ticket-price]
+(defn cost
   "Ex 3.1.3-4"
+  [ticket-price]
   (+ 180 (* 0.04 (attendees ticket-price))))
 
-(defn revenue [ticket-price]
+(defn revenue
   "Ex 3.1.3-4"
+  [ticket-price]
   (* (attendees ticket-price) ticket-price))
 
-(defn profit [ticket-price]
+(defn profit
   "Ex 3.1.3-4"
+  [ticket-price]
   (- (revenue ticket-price)
      (cost ticket-price)))
 
-(defn in->cm [in]
+
+(defn in->cm
   "Ex 3.3.1"
+  [in]
   (* 2.54 in))
 
-(defn ft->in [ft]
+(defn ft->in
   "Ex 3.3.1"
+  [ft]
   (* 12 ft))
 
-(defn yd->ft [yd]
+(defn yd->ft
   "Ex 3.3.1"
+  [yd]
   (* yd 3))
 
-(defn rd->yd [rd]
+(defn rd->yd
   "Ex 3.3.1"
+  [rd]
   (* 5.5 rd))
 
-(defn fl->rd [fl]
+(defn fl->rd
   "Ex 3.3.1"
+  [fl]
   (* 40 fl))
 
-(defn mi->fl [mi]
+(defn mi->fl
   "Ex 3.3.1"
+  [mi]
   (* 8 mi))
 
 (def ft->cm
@@ -105,30 +124,145 @@
 
 (def PI 3.1419)
 
-(defn area-circle [r]
+(defn area-circle
+  "Ex 3.3.2"
+  [r]
   (* PI (* r r)))
 
-(defn perimeter-circle [r]
+(defn perimeter-circle
+  "Ex 3.3.2"
+  [r]
   (* 2 PI r))
 
-(defn vol-cylinder [r h]
+(defn vol-cylinder
+  "Ex 3.3.2"
+  [r h]
   (* h (area-circle r)))
 
-(defn area-ring [inner outer]
+(defn area-ring
+  "Ex 3.3.3-4"
+  [inner outer]
   (- (area-circle outer)
      (area-circle inner)))
 
-(defn area-pipe [inner-r h thickness]
+(defn area-pipe
+  "Ex 3.3.3-4"
+  [inner-r h thickness]
   (let [outer-r (+ inner-r thickness)]
     (+ (* 2 (area-ring inner-r outer-r))
        (* h (perimeter-circle inner-r))
        (* h (perimeter-circle outer-r)))))
 
-(defn area-cylinder [r h]
+(defn area-cylinder
+  "Ex 3.3.3"
+  [r h]
   (area-pipe 0 h r))
 
-(defn rocket-height [g t]
+(defn rocket-height
+  "Ex 3.3.5"
+  [g t]
   (* 0.5 g t t))
+
+(defn c->f
+  "Ex 2.2.1"
+  [t]
+  (+ (* t 1.8) 32))
+
+(def temp-I
+  "Ex 2.2.1"
+  (comp c->f f->c))
+
+
+(defn interval [left-f left-val right-f right-val]
+  (fn [x] (and (left-f left-val x) (right-f right-val x))))
+
+
+;; Naming convention intvl5-6 is (5, 6), intvl-5-6 is [5,6], intvl-5-6- is [5,6], etc
+;; Could go wicked crazy and make it a macro?
+
+(def intvl3-7- "Ex 4.2.1.1" (interval < 3 >= 7))
+
+(def intvl-3-7- "Ex 4.2.1.2" (interval <= 3 >= 7))
+
+(def intvl-3-9 "Ex 4.2.1.3" (interval <= 3 > 9))
+
+(def intvl1-3 "Ex 4.2.1.4" (interval < 1 > 3))
+
+(def intvl9-11 "Ex 4.2.1.4" (interval < 9 > 11))
+
+(defn intvl1-3&9-11 "Ex 4.2.1.4" [x] (or (intvl1-3 x) (intvl9-11 x)))
+
+(defn intvl-not1-3 "Ex 4.2.1.5" [x] (not (intvl1-3 x)))
+
+;; these are some math problems translated into clojure?
+;; 4.2.3
+(def problems-4-2-3
+  [(fn [x] (= 62 (+ 2 (* 4 x))))
+   (fn [x] (= 102 (* 2 x x)))
+   (fn [x] (= 462 (+ (* 4 x x) (* 6 x) 2)))])
+
+
+(defn interest
+  "Ex 4.4.1"
+  [amount]
+  (let [rate (cond
+               (<= amount 1000) 0.04
+               (<= amount 5000) 0.045
+               :else 0.05)]
+    (* amount rate)))
+
+(defn tax
+  "Ex 4.4.2"
+  [amount]
+  (let [rate (cond
+               (<= amount 240) 0
+               (<= amount 480) 0.15
+               :else 0.28)]
+    (* amount rate)))
+
+;; TANGENT AHEAD!
+
+(defn graduated-tax
+  "It's bugging me that you earn less making 481 than 480.... this function is
+  ugly though, it should recurse through the brackets?"
+  [amount]
+  (cond
+    (<= amount 240) 0
+    (<= amount 480) (+ (* 0.15 (- amount 240)) 0)
+    :else (+ (* 0.28 (- amount 480)) (graduated-tax 480))))
+
+(defn taxable
+  "Returns the taxable dollars ina a given bracket range
+  i.e. if I have 110 dollars, 10 dollars are taxable in the 100+ range"
+  [amount [lower upper]]
+  (max
+    (if upper
+      ((min (- amount lower) (- upper lower)))
+      (- amount lower))
+    0))
+
+(defn grad-tax
+  "A more generalized graduated-tax function."
+  [amount]
+  (let [brackets {0.0 [0 240] 0.15 [240 480] 0.28 [480]}]
+    (reduce-kv (fn [total rate bracket]
+                 (+ total (* rate (taxable amount bracket))))
+                 0
+                 brackets)))
+
+;; /TANGENT
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -145,3 +279,15 @@
   (fn [coll]
     (let [kv (zipmap (map g coll) coll)]
       (get kv (apply f (keys kv))))))
+
+
+
+
+
+
+
+
+
+
+
+
